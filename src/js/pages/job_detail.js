6685cf2066c39a665919aed18f7daf27463e60c2
keisuke_itoi <keisuke_itoi@bita.jp>
2019-10-29 22:04:15 +0900

    update：ブラウザバック時のキャッシュ対策を追加
    https://bitadev.backlog.jp/view/DEV_MEDICALOFFICE_JOB-22

--- src/js/pages/job_detail.js
+++ src/js/pages/job_detail.js
@@ -2,8 +2,20 @@ import JobDetail from '../module/api/module/JobDetail'
 
 // 定数
 const LOAD_EVENT = 'DOMContentLoaded'
+const PAGESHOW_EVENT = 'pageshow'
 
 window.addEventListener(LOAD_EVENT, () => {
   // 求人詳細情報のAPI呼び出し
   new JobDetail()
+})
+
+// ブラウザバック時のキャッシュ対策
+window.onbeforeunload = function() {
+}
+window.onunload = function() {
+}
+window.addEventListener(PAGESHOW_EVENT, event =>{
+  if (event.persisted) {
+    window.location.reload();
+  }
 })
