7f1e1e48ebdae19690300f6a3afd523c9df1bb63
keisuke_itoi <keisuke_itoi@bita.jp>
2019-11-13 16:30:15 +0900

    update：JobDetail.jsの処理方法を一覧と合わせるように修正、不要ファイルの削除
    https://bitadev.backlog.jp/view/DEV_MEDICALOFFICE_JOB-65

--- src/js/module/api/module/MetaReplace.js
+++ src/js/module/api/module/MetaReplace.js
@@ -5,37 +5,42 @@
 class MetaReplace {
   /**
    * @constructor
-   * @param {string} title 入れ込みたいタイトルの文字列
-   * @param {string} description 入れ込みたいディスクリプションの文字列
+   * @param {string} title 変更するタイトルの文字列
+   * @param {string} description 変更するディスクリプションの文字列
    */
   constructor(title, description) {
-    this.title = title
-    this.description = description
+    this.titleReplace(title)
+    this.descriptionReplace(description)
+    this.ogpReplace(title, description)
   }
 
   /**
    * @desc タイトルタグを変更する
+   * @param {string} title 変更するタイトルの文字列
    */
-  titleReplace() {
-    document.title = `${this.title}｜医療事務職の求人サービスなら【M.PLAT 求職支援】`
+  titleReplace(title) {
+    document.title = `${title}｜医療事務職の求人サービスなら【M.PLAT 求職支援】`
   }
 
   /**
    * @desc ページディスクリプションを変更する
+   * @param {string} description 変更するディスクリプションの文字列
    */
-  descriptionReplace() {
-    document.getElementsByName('description')[0].content = this.description
+  descriptionReplace(description) {
+    document.getElementsByName('description')[0].content = description
   }
 
   /**
    * @desc OGP情報を変更する
+   * @param {string} title 変更するタイトルの文字列
+   * @param {string} description 変更するディスクリプションの文字列
    */
-  ogpReplace() {
+  ogpReplace(title, description) {
     const headTagList = document.head.children
     const current = location.pathname + location.search
     Array.prototype.forEach.call(headTagList, elem => {
-      if (elem.getAttribute('property') === 'og:title') elem.content = `${this.title}｜医療事務職の求人サービスなら【M.PLAT 求職支援】`
-      if (elem.getAttribute('property') === 'og:description') elem.content = this.description
+      if (elem.getAttribute('property') === 'og:title') elem.content = `${title}｜医療事務職の求人サービスなら【M.PLAT 求職支援】`
+      if (elem.getAttribute('property') === 'og:description') elem.content = description
       if (elem.getAttribute('property') === 'og:url') elem.content = `https://job.mplat.jp${current}`
     })
   }
