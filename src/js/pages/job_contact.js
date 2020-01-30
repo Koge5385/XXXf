d4e1015bb666059614c823c567fe4432aba07daf
武石健太郎 <kentaro_takeishi@ledge.co.jp>
2020-01-30 15:57:44 +0900

    change:全体のtitleとdescriptionを修正

--- src/js/pages/job_contact.js
+++ src/js/pages/job_contact.js
@@ -1,14 +1,19 @@
 import JobContact from '../module/api/module/JobContact'
+import JobContactMetaReplace from '../module/api/module/JobContactMetaReplace'
 import ActivateSubmit from '../module/ActivateSubmit'
 import FormConfirm from '../module/FormConfirm'
 
 // 定数
 const LOAD_EVENT = 'DOMContentLoaded'
 const CLICK_EVENT = 'click'
+const PAGESHOW_EVENT = 'pageshow'
 const SUBMIT_TARGET_CLASS = '.js-async-jobContactSubmit-target'
 const JOBDETAILHREF_TARGET_CLASS = '.js-async-jobContactHref-target'
 
 window.addEventListener(LOAD_EVENT, () => {
+  // 求人問合せのmeta情報の変更
+  new JobContactMetaReplace()
+
   // submitボタンの活性化処理
   new ActivateSubmit()
 
